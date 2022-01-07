99ea217f6be7b265fe25412c7961a74a1a600cc3
Lucas Barbosa <lukebarbosa.github@gmail.com>
2022-01-07 17:29:41 -0300

    Validação de formulário

--- app/Http/Requests/ContatoRequest.php
+++ app/Http/Requests/ContatoRequest.php
@@ -13,7 +13,7 @@ class ContatoRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -23,8 +23,40 @@ class ContatoRequest extends FormRequest
      */
     public function rules()
     {
+        switch ($this->method()) {
+            case "POST": // CREATING A NEW REGISTER
+                return [
+                    'nome' => 'required|max:100',
+                    'email' => 'email|max:200|unique:contatos',
+                    'cpf' => 'required|min:11|max:11|unique:contatos',
+                    'telefone' => 'required|max15',
+                    'data_nascimento' => 'date_format: "d/m/Y"',
+                    'avatar' => 'nullable|sometimes|image|mimes:png,jpg,jpeg,gif',
+                ];
+                break;
+
+            case "PUT": // UPDATING A EXISTENT REGISTER
+                return [
+                    'nome' => 'required|max:100',
+                    'email' => 'email|max:200|unique:contatos, email,' . $this->id,
+                    'cpf' => 'nullable|min:11|max:11|unique:contatos, cpf,' . $this->id,
+                    'telefone' => 'required|max15',
+                    'data_nascimento' => 'date_format: "d/m/Y"',
+                    'avatar' => 'nullable|sometimes|image|mimes:png,jpg,jpeg,gif',
+                ];
+                break;
+
+            default:
+                break;
+        }
+    }
+    public function messages()
+    {
         return [
-            //
+            'nome.required' => 'O campo nome é obrigatório',
+            'email.email' => 'Informe um e-mail válido',
+            'cpf' => 'Informe apenas os números do CPF',
+            'data_nascimento' => 'O campo data deve ser no formato DD/MM/YYYY',
         ];
     }
 }
