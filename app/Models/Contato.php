ee5d3f5ad04e21429e67b571f7b7d20e3534fe09
Lucas Barbosa <lukebarbosa.github@gmail.com>
2022-01-07 17:30:30 -0300

    Helper e tratamento de campos do Contato

--- app/Models/Contato.php
+++ app/Models/Contato.php
@@ -11,4 +11,32 @@ class Contato extends Model
     protected $fillable = [
         'nome', 'email', 'cpf', 'telefone', 'data_nascimento', 'avatar'
     ];
+    // Accessor
+    public function getAvatarImageAttribute($value)
+    {
+        return $this->avatar == null ? asset('images/null.png') : asset($this->avatar);
+    }
+    public function getAvatarFilenameAttribute($value)
+    {
+        return substr($this->avatar, 30, strlen($this->avatar));
+    }
+    public function getDataNascimentoAttribute($value)
+    {
+        return dateFormatDatabaseScreen($value, 'screen');
+    }
+
+    // Mutator
+    public function setDataNascimentoAttribute($value)
+    {
+        $this->attributes['data_nascimento'] = dateFormatDatabaseScreen($value);
+    }
+    public function setAvatarAttribute($value)
+    {
+        $filename = substr(md5(rand(100000, 999999)), 0, 5) . '_' . $value->getClientOriginalName();
+        $filepath = 'public/uploads/' . date('Y') . '/' . date('m') . '/';
+        if ($value->isValid()) {
+            $path = $value->storeAs($filepath, $filename);
+        }
+        $this->attributes['avatar'] = str_replace('public', 'storage', $filepath) . $filename;
+    }
 }
